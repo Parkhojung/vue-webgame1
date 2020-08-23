cb4f9d2e84d954ef9a3881d0a7e9fbe1c5f3b9b3
Parkhojung <34734102+Parkhojung@users.noreply.github.com>
2020-08-24 07:18:44 +0900

    Add files via upload

---
+++ RockPaper.vue
@@ -0,0 +1,67 @@
+<template>
+    <div>
+        <div id="computer" :style="computedStyleObject"></div>
+        <div>
+            <button @oclick="onClickButton('바위')">바위</button>
+            <button @oclick="onClickButton('가위')">가위</button>
+            <button @oclick="onClickButton('보')">보</button>
+        </div>
+        <div>{{result}}</div>
+        <div>현재 {{score}}점</div>
+    </div>
+</template>
+
+<script>
+
+const rspCoords = {
+    바위: '0',
+    가위: '-142px',
+    보: '-274px',
+}
+
+let interval = null;
+export default {
+    data() {
+        return {
+            imgCoord: rspCoords.바위,
+            result: '',
+            score: 0,
+        }
+    },
+    computed :{
+        computedStyleObject(){
+            return {
+                background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${this.imgCoord} 0`,
+                width: '125px',
+                height: '200px'
+            }
+        }
+    },
+    methods: {
+        onClickButton(choice) {
+
+
+        }
+    },
+    mounted() {
+        console.log('mounted');
+        interval = setInterval(()=> {
+            if(this.imgCoord === rspCoords.바위){
+                this.imgCoord = rspCoords.가위;
+            }else if(this.imgCoord === rspCoords.가위){
+                this.imgCoord = rspCoords.보;
+            }else if(this.imgCoord === rspCoords.보){
+                this.imgCoord = rspCoords.바위;
+            }
+        }, 100);
+    },
+    beforeDestroy() {
+        clearInterval(interval);
+    }
+};
+</script>
+
+<style>
+
+
+</style>
---
+++ main.js
@@ -0,0 +1,5 @@
+import Vue from 'vue';
+
+import RockPaper from './RockPaper'
+
+new Vue(RockPaper).$mount('#root');
